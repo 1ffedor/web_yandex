4253583584e16e0f4c3967bc877a43f308aee3c4
ffedor <ffedor.ffedorov@gmail.com>
2022-02-12 20:32:27 +0300

    problem4

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# https://github.com/1ffedor/web_yandex
-
 from flask import Flask, url_for
 
 app = Flask(__name__)
@@ -28,7 +26,7 @@ def index():
                   <head>
                     <meta charset="utf-8">
                     <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                    <title>И на Марсе будут яблони цвести!</title>
+                    <title>Привет, Марс!</title>
                   </head>
                   <body>
                     <h1>И на Марсе будут яблони цвести!</h1>
@@ -51,6 +49,42 @@ def image_mars():
                         <p>Вот она, красная планета</p>
                       </body>
                     </html>'''
+
+
+@app.route('/promotion_image')
+def promotion_image():
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1 class="text-danger">Привет, Яндекс!</h1>
+                    <div><img src={url_for('static', filename='images/mars.jpg')} width="500" height="500" alt="Нет фото"></div>
+                    <div class="alert alert-secondary" role="alert">
+                      Человечество вырастает из детства.
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                      Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                      Мы сделаем обитаемыми безжизненные пока планеты.
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      И начнем с Марса!
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                      Присоединяйся!
+                    </div>
+                  </body>
+                </html>'''
+
 
 
 if __name__ == '__main__':
