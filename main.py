7b6919c41b6b20da6204f0306517247ffd9c2bf2
ffedor <ffedor.ffedorov@gmail.com>
2022-02-13 18:57:43 +0300

    problem1

--- main.py
+++ main.py
@@ -1,121 +1,15 @@
-from flask import Flask, url_for
+# https://github.com/1ffedor/web_yandex/blob/problem6
+
+from flask import Flask, url_for, render_template
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/')
-
-def default():
-    return f'''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                    <title>Миссия Колонизация Марса</title>
-                  </head>
-                  <body>
-                    <h1>Миссия Колонизация Марса</h1>
-                  </body>
-                </html>'''
-
-
-@app.route('/index')
-def index():
-    return f'''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                    <title>Привет, Марс!</title>
-                  </head>
-                  <body>
-                    <h1>И на Марсе будут яблони цвести!</h1>
-                  </body>
-                </html>'''
-
-
-@app.route('/image_mars')
-def image_mars():
-    return f'''<!doctype html>
-                    <html lang="en">
-                      <head>
-                        <meta charset="utf-8">
-                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                        <title>Привет, Марс!</title>
-                      </head>
-                      <body>
-                        <h1>Жди нас, Марс!</h1>
-                        <p><img src={url_for('static', filename='images/mars.jpg')} width="500" height="500" alt="Нет фото"></p>
-                        <p>Вот она, красная планета</p>
-                      </body>
-                    </html>'''
-
-
-@app.route('/promotion_image')
-def promotion_image():
-    return f'''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet" 
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
-                    crossorigin="anonymous">
-                    <title>Привет, Марс!</title>
-                  </head>
-                  <body>
-                    <h1 class="text-danger">Привет, Яндекс!</h1>
-                    <div><img src={url_for('static', filename='images/mars.jpg')} width="500" height="500" alt="Нет фото"></div>
-                    <div class="alert alert-secondary" role="alert">
-                      Человечество вырастает из детства.
-                    </div>
-                    <div class="alert alert-success" role="alert">
-                      Человечеству мала одна планета.
-                    </div>
-                    <div class="alert alert-secondary" role="alert">
-                      Мы сделаем обитаемыми безжизненные пока планеты.
-                    </div>
-                    <div class="alert alert-warning" role="alert">
-                      И начнем с Марса!
-                    </div>
-                    <div class="alert alert-danger" role="alert">
-                      Присоединяйся!
-                    </div>
-                  </body>
-                </html>'''
-
-
-
-@app.route('/choice/<planet_name>')
-def choice(planet_name):
-    return f'''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet" 
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
-                    crossorigin="anonymous">
-                    <title>Привет, Марс!</title>
-                  </head>
-                  <body>
-                    <h1>Моё предложение: {planet_name}</h1>
-                    <div class="alert alert-success" role="alert">
-                      На ней много необходимых ресурсов;
-                    </div>
-                    <div class="alert alert-secondary" role="alert">
-                      На ней есть вода и атмосфера;
-                    </div>
-                    <div class="alert alert-warning" role="alert">
-                      На ней есть большое магнитное поле;
-                    </div>
-                    <div class="alert alert-danger" role="alert">
-                      Наконец, она просто красива!
-                    </div>
-                  </body>
-                </html>'''
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('index.html', title=title)
 
 
 if __name__ == '__main__':
